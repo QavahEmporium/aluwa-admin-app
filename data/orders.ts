9a85b164fff9a64fc996564d9b64b6873c1b46e4
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-18 17:57:46 +0200

    feat: added mock data

---
+++ data/orders.ts
@@ -0,0 +1,59 @@
+// data/orders.ts
+export const orders = [
+  {
+    id: 101,
+    customerId: 1,
+    status: "Completed",
+    total: 120.5,
+    date: "2025-07-01",
+  },
+  {
+    id: 102,
+    customerId: 1,
+    status: "Pending",
+    total: 80.0,
+    date: "2025-07-10",
+  },
+  {
+    id: 103,
+    customerId: 2,
+    status: "Completed",
+    total: 150.0,
+    date: "2025-07-03",
+  },
+  {
+    id: 104,
+    customerId: 3,
+    status: "Cancelled",
+    total: 200.75,
+    date: "2025-07-04",
+  },
+  {
+    id: 105,
+    customerId: 3,
+    status: "Completed",
+    total: 300.0,
+    date: "2025-07-07",
+  },
+  {
+    id: 106,
+    customerId: 4,
+    status: "Completed",
+    total: 150.25,
+    date: "2025-07-02",
+  },
+  {
+    id: 107,
+    customerId: 5,
+    status: "Pending",
+    total: 220.0,
+    date: "2025-07-05",
+  },
+  {
+    id: 108,
+    customerId: 1,
+    status: "Completed",
+    total: 250.0,
+    date: "2025-07-12",
+  },
+];
