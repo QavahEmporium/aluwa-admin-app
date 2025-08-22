7cea3570831f6411fdcf5bdc4fb77dc53a17c81d
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-22 10:01:15 +0200

    feat: added mobile view for order items

--- app/(dashboard)/orders/[id]/page.tsx
+++ app/(dashboard)/orders/[id]/page.tsx
@@ -27,7 +27,7 @@ export default async function OrderDetailPage({
   const items = await listOrderItems(id);
 
   return (
-    <div className="p-4 md:p-6 max-w-5xl mx-auto">
+    <div className="md:p-4 md:p-6 max-w-5xl mx-auto">
       <div className="flex flex-col gap-4 md:flex-row md:justify-between md:items-center mb-3">
         <div className="flex flex-col mb-3">
           <Link href="/orders" className="underline block">
@@ -90,9 +90,11 @@ export default async function OrderDetailPage({
         <p className="text-gray-600">{order.shippingAddressId?.postalCode}</p>
       </div>
       {/* Order Items */}
-      <div className="bg-white border border-gray-200 shadow-sm rounded-xl p-4 mb-6">
+      <div className="bg-white border border-gray-200 shadow-sm rounded-xl py-4 px-3 md:p-4 mb-6">
         <h2 className="text-lg font-semibold mb-4">Items</h2>
-        <div className="overflow-x-auto">
+
+        {/* Desktop Table */}
+        <div className="hidden md:block overflow-x-auto">
           <table className="w-full border border-gray-200 rounded-lg overflow-hidden text-sm">
             <thead className="bg-gray-50">
               <tr>
@@ -113,6 +115,26 @@ export default async function OrderDetailPage({
               ))}
             </tbody>
           </table>
+        </div>
+
+        {/* Mobile Cards */}
+        <div className="md:hidden space-y-4">
+          {items.map((item: any) => (
+            <div
+              key={item._id}
+              className="border border-gray-200 rounded-lg p-3 shadow-sm"
+            >
+              <div className="flex justify-between items-center mb-2">
+                <span className="font-medium">{item.name}</span>
+                <span className="text-gray-700 text-sm">
+                  R {(item.price * item.quantity).toFixed(2)}
+                </span>
+              </div>
+              <div className="text-gray-600 text-sm">
+                Quantity: {item.quantity}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
