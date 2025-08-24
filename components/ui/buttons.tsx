3e8a79e419b81f0332cad407f64cd0830ab8faee
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-24 18:11:52 +0200

    refactor: updated Submit button UI

--- components/ui/buttons.tsx
+++ components/ui/buttons.tsx
@@ -24,7 +24,7 @@ export const SubmitButton = ({
         <button
           type="submit"
           disabled={isPending}
-          className="border border-black px-4 py-2 rounded hover:bg-black hover:text-white transition"
+          className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition text-center"
         >
           {name}
         </button>
