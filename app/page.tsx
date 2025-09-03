d2a73ce0d411909f24749b37773da18746484f3f
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-09-03 10:46:10 +0200

    refactor: updated the middleware

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/home/page.tsx (Server Component)
-import DashboardLayout from "@/components/(dashboard)/home/home-page";
-import { getStatsData } from "@/data/dashabord";
-
-export default async function HomePage() {
-  const stats = await getStatsData();
-
-  return <DashboardLayout stats={stats} />;
-}
